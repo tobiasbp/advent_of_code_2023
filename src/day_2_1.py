d72b0c0157cd06954cefbe527ebe4bda5bea4612
Tobias Balle-Petersen <tobiasbp@gmail.com>
2023-12-05 09:22:15 +0100

    Day 2,2 complete

--- src/day_2_1.py
+++ src/day_2_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from math import prod
 from pathlib import Path
 
 class GameData:
@@ -26,7 +27,7 @@ class GameData:
     def game_data(self):
         return self._game_data        
 
-    def get_games_max(self, red:int, green:int, blue:int):
+    def get_possible_games(self, red:int, green:int, blue:int):
         """
         Get a list of ids for games that could have been played if the
         bag contained the argument number of cubes of each color
@@ -34,10 +35,7 @@ class GameData:
         game_ids = []
 
         for i, game in enumerate(self.game_data, start = 1):
-            print("Games:", game)
             max_cubes_seen = self.max_cubes(game)
-            print("Max:", max_cubes_seen)
-            print()
             if max_cubes_seen.get('red', 0) <= red and max_cubes_seen.get('green', 0)  <= green and max_cubes_seen.get('blue', 0) <= blue:
                 game_ids.append(i)
 
@@ -55,9 +53,24 @@ class GameData:
                     max_cubes[color] = samples
 
         return max_cubes
+    
+    def get_needed_dice_products(self):
+        """
+        For each game, get the product of the number
+        of cubes needed to play the game.
+        """
+        r = []
+        for game in self.game_data:
+            r.append(prod(self.max_cubes(game).values()))
+        return r
+
 
 d = GameData('data/day_2.txt')
 
-print("Sum of playable game IDs:", sum(d.get_games_max(red=12, green=13, blue=14)))
+answer_2_1 = sum(d.get_possible_games(red=12, green=13, blue=14))
+assert answer_2_1 == 2810
+print("Sum of playable game IDs:", answer_2_1)
 
-      
+answer_2_2 = sum(d.get_needed_dice_products())
+assert answer_2_2 == 69110
+print("Sum of dice products:", answer_2_2)
